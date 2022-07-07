500480257b3c8cc4e83f25d7cd481f73ec3342f4
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-07 12:11:33 -0500

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
-  const {good, neutral, bad} = props
-  
-  return (
-    <>
-      <h2>Statistics:</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Average Score: {(good * 1) + (neutral * 0) + (bad * -1)}</p>
-      <p>Percent Positive: {(good / (good + neutral + bad)) * 100}</p>
-    </>
-  )
+  const {good, neutral, bad, allClicks} = props
+
+  if (allClicks.length === 0) {
+    return(
+      <>
+        <h2>Statistics:</h2>
+        <p>the app is used by pressing the buttons :0</p>
+      </>
+    )
+  } 
+  else {
+    return (
+      <>
+        <h2>Statistics:</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Average Score: {(good * 1) + (neutral * 0) + (bad * -1)}</p>
+        <p>Percent Positive: {(good / (good + neutral + bad)) * 100}</p>
+      </>
+    )
+  }
 }
 
 const App = () => {
@@ -20,18 +30,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   return (
     <div>
       <h2>Give Feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={() => {
+        setAll(allClicks.concat('G'))
+        setGood(good + 1)}
+      }>good</button>
+      <button onClick={() => {
+        setAll(allClicks.concat('N'))
+        setNeutral(neutral + 1)}
+      }>neutral</button>
+      <button onClick={() => {
+        setAll(allClicks.concat('B'))
+        setBad(bad + 1)}
+      }>bad</button>
       <hr />
       <Statistics 
         good = {good}
         neutral = {neutral}
         bad = {bad}
+        allClicks = {allClicks}
       />
     </div>
   )
