dabc84aa6b0e1bb308cce640328b630aff54c5f0
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-10 13:13:29 -0500

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from "./components/Filter"
 import Form from "./components/Form"
 import List from "./components/List"
+import axios from "axios"
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [allPersons, setAllPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [allPersons, setAllPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        console.log('promise fufilled')
+        setAllPersons(res.data)
+      })
+  }, [])
+  console.log(`render ${allPersons.length} names`)
 
   const addName = (event) => {
     event.preventDefault()
