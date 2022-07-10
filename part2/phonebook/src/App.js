cf7248a939e8f426942e1e6f18f3ffb1bb803b90
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-10 09:41:19 -0500

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = (props) => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      phoneNumber: '(345) 126-7890'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
-  const [showAll, setShowAll] = useState(true)
+  const [search, setSearch] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -38,19 +39,29 @@ const App = (props) => {
     setNewPhoneNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+
   const Person = (props) => {
     return (
       <li key={props.id}>{props.name}: {props.number}</li>
     )
   }
 
-  const namesToShow = showAll 
-  ? persons 
-  : persons.filter(note => note.important)
+  const namesToShow = search === "" 
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with</p> <input 
+                          value={search}
+                          onChange={handleSearch}
+                        />
       <form onSubmit={addName}>
         <div>
           name: <input 
@@ -68,13 +79,10 @@ const App = (props) => {
       </form>
       <h2>Numbers</h2>
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
       </div>
       <ul>
         {namesToShow.map(persons => 
-            <Person key={persons.id} name={persons.name} number={persons.phoneNumber}  />
+            <Person key={persons.id} name={persons.name} number={persons.number}  />
         )}
       </ul>
     </div>
